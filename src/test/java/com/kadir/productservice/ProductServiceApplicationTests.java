2ab8cf91e2879ac9a22483797f8a3b0290e98198
abdulkadir <abdulkadiro@cloudcan.com>
2026-01-15 08:04:29 +0300

    Initial commit

---
+++ src/test/java/com/kadir/productservice/ProductServiceApplicationTests.java
@@ -0,0 +1,63 @@
+package com.kadir.productservice;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.kadir.productservice.dto.ProductRequest;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.testcontainers.containers.MongoDBContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+import java.math.BigDecimal;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@Testcontainers
+@AutoConfigureMockMvc
+class ProductServiceApplicationTests {
+
+    @Container
+    static MongoDBContainer mongoDBContainer = new MongoDBContainer("mongo:4.4.2");
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    static void setProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.data.mongodb.host", mongoDBContainer::getReplicaSetUrl);
+    }
+
+    @Test
+    void shouldCreateProduct() throws JsonProcessingException {
+
+        ProductRequest productRequest = getProductRequest();
+        String s = objectMapper.writeValueAsString(productRequest);
+
+        try {
+            mockMvc.perform(MockMvcRequestBuilders.post("/api/v1/product")
+                            .contentType(MediaType.APPLICATION_JSON)
+                            .content(s))
+                    .andExpect(status().isCreated());
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private ProductRequest getProductRequest() {
+        return ProductRequest.builder().name("iphone 13")
+                .description("iphone 13 pro max")
+                .price(BigDecimal.valueOf(1200))
+                .build();
+    }
+
+}
